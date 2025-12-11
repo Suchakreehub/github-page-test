12ae30c48fd1daf20d0a3370d2da1dcd9d3b7b61
Suchakree Kumgade <suchakree.kg@gmail.com>
2025-12-11 16:46:13 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+
+export default defineConfig({
+  site: "https://suchakreehub.github.io/",
+  base: "github-page-test",
+});
